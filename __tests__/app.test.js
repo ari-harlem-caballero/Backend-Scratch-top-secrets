75463240e925b18f59ad96fcfa04fded3a7d67eb
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-03-30 12:50:42 -0700

    added first test for signup user with POST

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,20 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('alchemy-app routes', () => {
+describe('auth-api routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('should sign up user with POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/users')
+      .send({ username: 'ariIsBest', password: 'suckanegg' });
+
+    expect(res.body).toEqual({ id: expect.any(String), username: 'ariIsBest' });
   });
 });
